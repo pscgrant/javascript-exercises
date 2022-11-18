a46fc9692e7d0f20fdb258552506d4afe9ef20c6
Sean Grant <pscgrant95@gmail.com>
2022-11-18 13:28:27 -0800

    tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  var splitString = str.split(""); // This puts the string into an array
+  var reverseArray = splitString.reverse(); // reverses the original array
+  var joinArray = reverseArray.join("");// joins the array into string
+  return joinArray;
 
 };
 
