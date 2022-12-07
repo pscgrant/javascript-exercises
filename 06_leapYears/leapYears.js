e48adf6f684df5480ac526bcd2b66059cc87575e
Sean Grant <pscgrant95@gmail.com>
2022-12-07 15:06:23 -0800

    completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+//    if ((!years % 100) == 0) return false;
+//    if ((!years % 100) == 0 && (years % 400) == 0) return true;
+//    if ((years % 4) == 0 ) return true;
+//    console.log(years);
+//  My first thought on how to do this
 
+    return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
 };
 
 // Do not edit below this line
