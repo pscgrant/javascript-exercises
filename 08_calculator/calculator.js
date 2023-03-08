4ba42085a0f6be8beecb41b9273ab3ab38bb4457
Sean Grant <pscgrant95@gmail.com>
2023-03-08 15:23:10 -0800

    finshed problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 += num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((partialSum, a) => partialSum + a, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((num1, num2) => num1 * num2);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) 
+return 1;
+for (let i = num - 1; i >= 1; i--) {
+num *= i;
+}
+return num;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
